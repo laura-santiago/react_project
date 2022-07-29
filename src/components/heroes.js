73fbd6e478d07cbf334ef75db10c186dee778a41
laura-santiago <laurasantiagovera@alumno.ieselrincon.es>
2022-07-29 20:16:57 +0100

    with heros and button

--- src/components/heroes.js
+++ src/components/heroes.js
@@ -6,30 +6,39 @@ export default class Heroes extends Component {
     super(props)
   
     this.state = {
-       heroes: ['Batman','Superman','Spiderman'],
-       newHero:''
+       heroes: [{name:'Batman', description:'black'},
+                {name:'Superman', description:'blue'},
+                {name:'Spiderman', description:'red'}],
+       newHero:{name:'', description:''}
     }
   } 
 
   addHero(){
-    this.setState({heroes: [...this.state.heroes, this.state.newHero]})
+    this.setState({heroes: [...this.state.heroes, this.state.newHero], newHero:''})
   }
 
   //called any time sth is written in the input
   handleChange(event){
-    this.setState({newHero: event.target.value});
-    console.log(event.target.name);
+    let myHero = this.state.newHero;
+    if (event.target.name === "nameInput"){
+        myHero.name = event.target.value;
+    } else if (event.target.name === "descriptionInput") {
+            myHero.description = event.target.value;
+    }
+    this.setState({newHero: myHero});
   }
 
   render() {
     const listHeroes = this.state.heroes.map(
-      (hero, i) => <li key={i} className="list-group-item">{hero}</li>)
+      (hero, i) => <li key={i} className="list-group-item"><strong>{hero.name}</strong><br/> {hero.description} <a name="" id="" class="btn btn-danger float-right" href="#" role="button">eliminar</a> </li>)
       
     return (
       <div className="container">
         <div className="form-group">
-          <label for="">Nombre del Heroe</label>
-          <input type="text" className="form-control" name="inputHero" id="" aria-describedby="helpId" placeholder="" value={this.state.newHero} onChange={(event)=>{this.handleChange(event)}}/>
+          <label for="">Nombre del Héroe</label>
+          <input type="text" className="form-control" name="nameInput" id="" aria-describedby="helpId" placeholder="" value={this.state.newHero.name} onChange={(event)=>{this.handleChange(event)}}/>
+          <label for="">Descripción</label>
+          <input type="text" className="form-control" name="descriptionInput" id="" aria-describedby="helpId" placeholder="" value={this.state.newHero.description} onChange={(event)=>{this.handleChange(event)}}/>
           <button type="button" className="btn btn-primary" onClick={() => this.addHero()}>Añade héroe</button>
         </div>
 
